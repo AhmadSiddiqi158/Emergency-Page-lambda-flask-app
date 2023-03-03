05566afbb21a077c766c4d910787252015170151
Ahmad Siddiqi <ahmad.siddiqi158@gmail.com>
2023-03-03 16:08:40 -0500

    Works well Locally

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,16 @@
-import json
+import boto3
 
-# import requests
-
+ddb = boto3.resource("dynamodb")
+table = ddb.Table('Emergency')
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    #Adding the emergency to the DynamoDB table
+    table.put_item(Item=event)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    #Get all the emergencies that have been reported
+    emergencies = table.scan()['Items']
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    return emergencies
 
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+ 
+ 
